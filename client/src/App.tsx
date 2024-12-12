5d901f5d59175d46bd0eede9e9de637ad82708e9
ermalsalihaj <ermalsalihaj03@gmail.com>
2024-12-12 23:40:07 +0100

    Integrate Ant Design for UI components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
+import { Button, Input } from "antd"
+
 function App() {
 
   return (
-    <div className="h-screen flex justify-center items-center bg-green-700">
-      <h1 className="text-white text-3xl font-bold">Ballina</h1>
+    <div className="h-screen flex justify-center items-center flex-col gap-5">
+      <h1 className="text-gray-500 text-3xl font-bold">Ballina</h1>
+
+      <Button type="primary">Primary Button</Button>
+
+      <Button >Default Button</Button>
+
+      <Input placeholder="Basic Usage"/>
     </div>
   )
 }
