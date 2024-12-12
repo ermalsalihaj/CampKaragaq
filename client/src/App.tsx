71ab0457f3629c95a785f45663ec3a13324a23cb
ermalsalihaj <ermalsalihaj03@gmail.com>
2024-12-12 23:28:24 +0100

    Setup Tailwind CSS for styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
 
   return (
-    <div>
-      <h1>Ballina</h1>
+    <div className="h-screen flex justify-center items-center bg-green-700">
+      <h1 className="text-white text-3xl font-bold">Ballina</h1>
     </div>
   )
 }
