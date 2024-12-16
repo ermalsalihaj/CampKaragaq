877d838d5840c7c94aeb069765f1166148b16c49
ermalsalihaj <ermalsalihaj03@gmail.com>
2024-12-16 23:07:40 +0100

    Establish MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const { connectMongoDB } = require("./config/db-config");
 
 const app = express();
+
+require("dotenv").config();
+
+connectMongoDB();
 
 const port = process.env.PORT || 5000;
 
