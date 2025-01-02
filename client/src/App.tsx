5d75be0f16cb329483753a8b346eb4492fd63786
ermalsalihaj <ermalsalihaj03@gmail.com>
2025-01-02 21:53:40 +0100

    Setup routes and Page Title component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,9 @@ import HomePage from "./pages/private/home"
 import ProfilePage from "./pages/private/profile"
 import PublicLayout from "./layouts/public-layout"
 import PrivateLayout from "./layouts/private-layout"
+import EventsPage from "./pages/private/admin/events"
+import CreateEventPage from "./pages/private/admin/events/create"
+import EditEventPage from "./pages/private/admin/events/edit"
 
 function App() {
 
@@ -17,6 +20,9 @@ function App() {
           <Route path="/register" element={<PublicLayout> <RegisterPage /></PublicLayout>} />
           <Route path="/" element={<PrivateLayout> <HomePage /> </PrivateLayout>} />
           <Route path="/profile" element={<PrivateLayout> <ProfilePage /></PrivateLayout>} />
+          <Route path="/admin/events" element={<PrivateLayout> <EventsPage /></PrivateLayout>} />
+          <Route path="/admin/events/create" element={<PrivateLayout> <CreateEventPage /></PrivateLayout>} />
+          <Route path="/admin/events/edit/:id" element={<PrivateLayout> <EditEventPage /></PrivateLayout>} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
