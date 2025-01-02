96fc46674ed1cb645d5ddf2025ca2b702d04912f
ermalsalihaj <ermalsalihaj03@gmail.com>
2025-01-02 20:01:12 +0100

    Add Spinner component

--- client/src/layouts/private-layout.tsx
+++ client/src/layouts/private-layout.tsx
@@ -5,18 +5,24 @@ import Sidebar from "./sidebar";
 import { getCurrentUser } from "../api-services/users-service";
 import { message } from "antd";
 import usersGlobalStore, { UsersStoreType } from "../store/users-store";
+import Spinner from '../components/spinner'
+
 
 function PrivateLayout({ children }: { children: React.ReactNode }) {
     const [showContent, setShowContent] = useState(false);
+    const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
     const { setCurrentUser, currentUser }: UsersStoreType = usersGlobalStore() as UsersStoreType;
 
     const getData = async () => {
         try {
+            setLoading(true);
             const response = await getCurrentUser();
             setCurrentUser(response.data)
         } catch (error: any) {
             message.error(error.response.data.message || error.message)
+        } finally {
+            setLoading(false)
         }
     };
 
@@ -31,11 +37,17 @@ function PrivateLayout({ children }: { children: React.ReactNode }) {
         }
     }, []);
 
+    if (loading) {
+        return <div className="flex items-center justify-center h-screen">
+            <Spinner />
+        </div>
+    }
+
     return (
         showContent && currentUser && (
             <div className="flex lg:flex-row flex-col gap-5 h-screen">
                 <Sidebar />
-                <div className="flex-1">
+                <div className="flex-1 px-2 lg:mt-10">
                     {children}
                 </div>
             </div>
