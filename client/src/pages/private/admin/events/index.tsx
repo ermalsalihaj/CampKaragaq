24aaf306ea69571bf83133e7440d15bcfbc25fab
ermalsalihaj <ermalsalihaj03@gmail.com>
2025-01-14 23:55:18 +0100

    Implement Fetch and Display Events functionality

--- client/src/pages/private/admin/events/index.tsx
+++ client/src/pages/private/admin/events/index.tsx
@@ -1,9 +1,37 @@
-import { Button } from "antd"
+import { Button, message } from "antd"
 import PageTitle from "../../../../components/page-title"
 import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { getEvents } from "../../../../api-services/events-service";
 
 function EventsPage() {
+    const [events, setEvents] = useState([])
+    const [loading, setLoading] = useState(true)
     const navigate = useNavigate();
+
+    const getData = async () => {
+        try {
+            setLoading(true);
+            const response = await getEvents()
+            setEvents(response.data)
+        } catch (error) {
+            message.error('Failed to fetch events')
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+    const columns = [
+        {
+            title: 'Event Name',
+            dataIndex: "name"
+        }
+    ]
+
     return (
         <div>
             <div className="flex justify-between items-center">
